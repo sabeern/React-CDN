c25cc357ce4aae37a7dae251c2acebad9455893b
sabeern <sabeerneyyan@gmail.com>
2023-02-26 12:59:43 +0530

    Injected multiple elements using react

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
-const heading = React.createElement("h1", {}, "Hello React");
+const heading = React.createElement(
+  "h1",
+  { title: "React heading", style: { color: "red", fontFamily: "Arial" } },
+  "Hello React"
+);
+const heading2 = React.createElement("h2", {}, "Second heading");
+const container = React.createElement("div", { className: "container" }, [
+  heading,
+  heading2,
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(container);
